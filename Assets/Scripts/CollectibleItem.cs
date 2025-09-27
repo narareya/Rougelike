30f60287a13f4fbbf9fd1436dae534ae7e8a543d
kusumadoni77 <kusumadoni77@gmail.com>
2025-09-27 10:52:43 +0700

    revoke item slot script

--- Assets/Scripts/CollectibleItem.cs
+++ Assets/Scripts/CollectibleItem.cs
@@ -6,20 +6,31 @@ public class CollectibleItem : MonoBehaviour
     [SerializeField] private Sprite itemSprite;
     [SerializeField] private int quantity = 1;
 
+    private InventoryManager inventoryManager;
+
     void Start()
     {
-
+        inventoryManager = FindAnyObjectByType<InventoryManager>();
+        if (inventoryManager == null)
+        {
+            Debug.LogError("Inventory manager has not assigned");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            // Add the item to the inventory
-            InventoryManager.AddItem(itemName, itemSprite, quantity);
+            if (inventoryManager != null)
+            {
+                // Add the item to the inventory
+                inventoryManager.AddItem(itemName, quantity, itemSprite);
 
-            // Destroy the collectible item from the scene
-            Destroy(gameObject);
+                // Destroy the collectible item from the scene
+                Destroy(gameObject);
+
+            }
+
         }
     }
 }
